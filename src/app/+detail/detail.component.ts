35cfa3e913d36b8681daf698fb8633bc8ea1cb2e
Taghurei <gauthier.lacroix@supelec.fr>
2018-04-12 16:48:52 +0200

    test1

---
+++ src/app/+detail/detail.component.ts
@@ -0,0 +1,55 @@
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+/**
+ * We're loading this component asynchronously
+ * We are using some magic with es6-promise-loader that will wrap the module with a Promise
+ * see https://github.com/gdi2290/es6-promise-loader for more info
+ */
+
+console.log('`Detail` component loaded asynchronously');
+
+@Component({
+  selector: 'detail',
+  template: `
+  <!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBd6aSTFJ_cvRJqm7WhX6RBRnKJ9tkluAk&callback=initMap">
+    </script>
+  </body>
+</html>
+  `,
+})
+export class DetailComponent implements OnInit {
+
+  public ngOnInit() {
+    console.log('hello `Detail` component');
+  }
+
+}
